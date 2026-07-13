2202213f958ea2d903ea70019a29679b26488d84
P Aswin <aswinappu35810@gmail.com>
2026-07-13 14:09:38 +0530

    add  rupees to  dollar

---
+++ task2.js
@@ -0,0 +1,5 @@
+let inr= parseInt(prompt("enter your rupees: "))
+let d=inr/86
+
+alert("converted rupees in dollar: "+(Math.floor(d))+"$")
+console.log("indian rupees: ",inr,"₹"," the converted rupees to dollar",d)
