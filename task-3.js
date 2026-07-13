78b0b14143ac743528c4403f64f1ed41e584ad47
P Aswin <aswinappu35810@gmail.com>
2026-07-13 19:16:19 +0530

    kilo meter and mileag  and calculate total cost of pertrol

---
+++ task-3.js
@@ -0,0 +1,9 @@
+let k=parseInt(prompt("enter the travelled km: "))
+let m=parseInt(prompt("enter the Mileag: "))
+let ppl = 100
+
+// cost=k/m
+// let tc=cost*ppl
+mp=ppl/m
+ap=mp*k
+alert("the price of travelled price: "+Math.floor(ap)+"₹")
