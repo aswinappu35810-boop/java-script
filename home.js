321f7a298ac221a48f35488a7aedbf3baceb3230
P Aswin <aswinappu35810@gmail.com>
2026-07-13 13:11:51 +0530

    practicing prompt fuction in java script

--- home.js
+++ home.js
@@ -1,5 +1,12 @@
-let a=10
-let b=20
-let temp=a
-a=b,b=temp
-console.log("the value a: ",a," the value of b: ",b)
+// let a=10
+// let b=20
+// let temp=a
+// a=b,b=temp
+// console.log("the value a: ",a," the value of b: ",b)
+
+
+let name=prompt("enter your Name: ")
+let Age=parseInt(prompt("enter your Age: "))
+let Email=prompt("enter your Mail: ")
+
+console.log("Name: ",name,"Age: ",Age,"Your mail is: ",Email)
