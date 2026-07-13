406d9f3e85f1efecf6d0e3b636ef80d81dad6679
P Aswin <aswinappu35810@gmail.com>
2026-07-13 13:27:10 +0530

    calculation using prompt fuction

--- home.js
+++ home.js
@@ -5,8 +5,22 @@
 // console.log("the value a: ",a," the value of b: ",b)
 
 
-let name=prompt("enter your Name: ")
-let Age=parseInt(prompt("enter your Age: "))
-let Email=prompt("enter your Mail: ")
+// let name=prompt("enter your Name: ")
+// let Age=parseInt(prompt("enter your Age: "))
+// let Email=prompt("enter your Mail: ")
 
-console.log("Name: ",name,"Age: ",Age,"Your mail is: ",Email)
+// console.log("Name: ",name,"Age: ",Age,"Your mail is: ",Email)
+
+
+let a=parseInt(prompt("enter the first number: "))
+let b=parseInt(prompt("enter the Second number: "))
+let add =a+b // add
+let sub =a-b //subtract
+let mul =a*b //multiply
+let div =a/b //devison
+ console.log("the addition is: ",add)
+ console.log("the subtraction is: ",sub)
+ console.log("the multiplication is: ",mul)
+ console.log("the divison is: ",parseFloat(div))
+
+
