9566307007c9e24737c8b5295f1f52e7a34599d1
P Aswin <aswinappu35810@gmail.com>
2026-07-14 19:48:19 +0530

    added the task of chocolate task

---
+++ chocolates.js
@@ -0,0 +1,7 @@
+let c = parseInt(prompt("Enter the total no of chocolates: "))
+
+let box = Math.floor(c/12)
+alert("the no of box: "+box)
+
+let remaining = c % 12
+alert("the remaining chocolate: "+remaining)
