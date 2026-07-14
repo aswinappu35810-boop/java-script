50f273d30c5adbf368ea23e5adf27637b8b45752
P Aswin <aswinappu35810@gmail.com>
2026-07-14 19:56:17 +0530

    added elevator task

---
+++ elevator.js
@@ -0,0 +1,5 @@
+let people = parseInt(prompt("Enter the total no of People: "))
+let trp = Math.floor(people/8)
+alert("The no of trip: "+trp)
+let remaining = people % 8
+alert("enter the remaing people: "+remaining)
