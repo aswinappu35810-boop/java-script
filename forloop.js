de78d92a842c868635226884140ece0472709f64
P Aswin <aswinappu35810@gmail.com>
2026-07-17 13:15:10 +0530

    for loop for arrays push and print even numbers

---
+++ forloop.js
@@ -0,0 +1,18 @@
+let numbers=[]
+
+let length = parseInt(prompt("entert the lenght of array: "))
+
+for (i=0 ; i<length; i++)
+{
+    let number = prompt("enter the number to be added in array: ")
+    numbers.push(number)
+}
+
+console.log(numbers)
+
+for(i=0; i<numbers.length; i++){
+    if(numbers[i]%2 == 0)
+    {
+        console.log("printed the even numbers",numbers[i])
+    }
+}
