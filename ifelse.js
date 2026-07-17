9e958be10653a587bc869236f29c1e09cab56115
P Aswin <aswinappu35810@gmail.com>
2026-07-17 13:03:14 +0530

    if else added

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+let age= parseInt(prompt("enter the your age"))
+
+if(age>=18 && age<=60)
+{
+    alert("you are eligible to vote")
+}
+else if (age>100){
+    alert("you are goat")
+}
+else{
+    alert("you are not eligible to vote")
+}
