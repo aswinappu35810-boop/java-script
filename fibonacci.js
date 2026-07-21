cdaa60248ce016a164690df33e2b3a1f8cbe150c
P Aswin <aswinappu35810@gmail.com>
2026-07-21 14:06:39 +0530

    completed the fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,26 @@
-let x = parseInt(prompt("how many times want to generate: "))
-let y =  0
-for(i=1;)
+let terms = parseInt(prompt("how many times want to generate: "))
+let y =  1
+let x =  0
+let num
+
+if(terms>=0){
+console.log(x);
+console.log(y);
+
+for(let i=1; i<=terms; i++)
+    {        
+        num = x + y
+        x=y
+        y = num
+
+        console.log(num)
+
+    }
+    
+}
+else
+{
+    console.log("negative value are not allowed");
+}
+ 
+
