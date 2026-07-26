5b18b094eb9acfa77cf2165bd066d2c46a4eaf42
P Aswin <aswinappu35810@gmail.com>
2026-07-26 17:52:36 +0530

    completed the logest win streak challenge

--- longestwin.js
+++ longestwin.js
@@ -1,28 +1,32 @@
-let array=[1,1,0,1,1,1,0,1,1,1,0,1,1]
-let total = 0
-let count = 0 
-let x= 0
-let y=0
-let win=0
-for(i=0; i<array.length; i++)
-{
-    if(array[i]==1)
-        {
-            count=count+1
-            total=count
+let data=[1,1,1,0,1,1,1,1]
+let win=1
+let total=0
+let startstreak=0
+let longstreak=0
+let startindex=0
+for(let i=0;i<data.length-1;i++){
+    if(data[i]==1 && data[i]==data[i+1]){
+          if(win==1){
+            startstreak= i
+            total=total+1
         }
-        else
-            {
-                count=0
-                // console.log(array[i])
-            }
-            
-            if (y<total)
-                {
-                    y=total
-                }
+        
+        win=win+1
+      
+        if(win > longstreak){
+            longstreak=win
+            startindex=startstreak
+        }
+    }
+    else{
+        win=1
+    }
+   
+    
 }
 
-
-console.log(x)
-// console.log(win)
+console.log("Longest streak: ",longstreak);
+console.log("Total streaks: ",total);
+if(total>=1){
+console.log("Starting index of longest streak: ",startindex);
+}
