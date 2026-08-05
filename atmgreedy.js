73d7c32b5a4eaccebeae3474c23495765d496b86
P Aswin <aswinappu35810@gmail.com>
2026-08-05 19:49:50 +0530

    completed the atm greedy method

--- atmgreedy.js
+++ atmgreedy.js
@@ -2,10 +2,8 @@ let notes=[500,200,100,50]
 let quantity=[2,3,5,10]
 let fund = parseInt(prompt("Enter the fund needed to be withdraw"))
 let x = 0 
-let found = false
-let remainingfund = 0
 let remainingnote = 0
-
+let dispensed = [0,0,0,0]
 for(i=0;i<notes.length && i<quantity.length;i++){
     
     console.log("Initial quantity"+notes[i]+"-->"+quantity[i])
@@ -19,27 +17,33 @@ for(var i=0;i<notes.length && i<quantity.length;i++)
         {
             quantity[i]=quantity[i]-x
             remainingnote = x
+            dispensed[i] = x
             fund =fund-(notes[i]*remainingnote)
             // console.log("Remaining quantity"+notes[i]+"-->"+quantity[i])
         }
     else
         {
             remainingnote = quantity[i]
+            dispensed[i] = quantity[i]
             fund = fund-(notes[i]*remainingnote)
             quantity[i] = 0
         }
-    if(remainingnote>0)
-    {
-        console.log("Dispensed Notes: " + notes[i] + " x " + remainingnote)
-    }
-        console.log("Remaining quantity: "+notes[i]+"-->"+quantity[i])
-    if(fund == 0)
-        {
-            found=true
-        }
 
 }
+
+
 if(fund != 0)
 {
     console.log("Transaction Failed")
+}
+else
+{
+    for(i=0;i<notes.length && i<quantity.length;i++)
+    {
+        console.log("Dispensed notes"+notes[i]+" x "+dispensed[i])
+    }
+    for(i=0;i<notes.length && i<quantity.length;i++)
+    {
+        console.log("Remaining quantity"+notes[i]+"-->"+quantity[i])
+    }
 }
