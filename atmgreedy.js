6bbc120d4d5763f7c721389689061291d9c27a75
P Aswin <aswinappu35810@gmail.com>
2026-08-05 19:36:58 +0530

    updated version

--- atmgreedy.js
+++ atmgreedy.js
@@ -2,28 +2,44 @@ let notes=[500,200,100,50]
 let quantity=[2,3,5,10]
 let fund = parseInt(prompt("Enter the fund needed to be withdraw"))
 let x = 0 
-let y = 0 
+let found = false
 let remainingfund = 0
 let remainingnote = 0
 
-for(i=0;i<notes.length;i++)
+for(i=0;i<notes.length && i<quantity.length;i++){
+    
+    console.log("Initial quantity"+notes[i]+"-->"+quantity[i])
+}
+
+for(var i=0;i<notes.length && i<quantity.length;i++)
 {
     x = Math.floor(fund/notes[i])
-    y = fund % notes[i]
-    for(j=0;j<quantity.length;j++)
-    {
-        if(quantity[j]>=x)
-        {
-            quantity[j]=quantity[j]-x
-            remainingnote = x
-            remainingfund =fund-(notes[i]*y)
-        }
-        else
-        {
-            j = 0
-            remainingnote = quantity[j]
-            remainingfund = fund-(notes[i]*j)
-        }
-    }
 
+    if(quantity[i]>=x)
+        {
+            quantity[i]=quantity[i]-x
+            remainingnote = x
+            fund =fund-(notes[i]*remainingnote)
+            // console.log("Remaining quantity"+notes[i]+"-->"+quantity[i])
+        }
+    else
+        {
+            remainingnote = quantity[i]
+            fund = fund-(notes[i]*remainingnote)
+            quantity[i] = 0
+        }
+    if(remainingnote>0)
+    {
+        console.log("Dispensed Notes: " + notes[i] + " x " + remainingnote)
+    }
+        console.log("Remaining quantity: "+notes[i]+"-->"+quantity[i])
+    if(fund == 0)
+        {
+            found=true
+        }
+
+}
+if(fund != 0)
+{
+    console.log("Transaction Failed")
 }
