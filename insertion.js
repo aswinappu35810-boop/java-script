c32837e32824b65960877113bb77a1d9cdf64565
P Aswin <aswinappu35810@gmail.com>
2026-08-10 11:29:48 +0530

    completed the  js of insertion

--- insertion.js
+++ insertion.js
@@ -4,11 +4,13 @@ let a=[1,2,3,4,5]
 let element = parseInt(prompt("entert the element to enter: "))
 let position = parseInt(prompt("enter the position: "))
 let next = 0
-for(i=position;i<a.length+1;i++)
+for(i=a.length; i>=position; i--)
+{ 
+a[i]=a[i-1]
+if(i==position)
 {
-next=a[i]
-a[i]=element
-element = next
+    a[i] = element
+}
 
 }
 console.log(a);
